e165ccc5729f8be8da9e383f50c59423c8e840ae
JayroGG <jaxjayro@gmail.com>
2023-02-01 17:08:48 -0600

    Added Intl for dates

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 const MovieCard = ({title, genre, release_date}) => {
     const [disabled, setDisabled] = useState(false)
-
+    const date = new Intl.DateTimeFormat('en-US').format(new Date(release_date))
     const handleClick = () => {
         setDisabled(!disabled)
     }
@@ -13,7 +13,7 @@ const MovieCard = ({title, genre, release_date}) => {
         <div onClick={handleClick} className={classesNames}>
             <span >Title: {title}</span>
             <span>Genre: {genre}</span>
-            <span>Release Date: {release_date}</span>
+            <span>Release Date: {date}</span>
         </div>
     </>
 }
