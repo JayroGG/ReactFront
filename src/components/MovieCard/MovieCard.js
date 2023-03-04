e59cd666db24988091734e6f2d2f8adad501daff
Jayro Gómez <jaxjayro@gmail.com>
2023-03-04 01:26:50 -0700

    Now the smaller animation only works on mouse leave

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,18 @@
 import './MovieCard.css'
 import { Link } from 'wouter'
+import { useState } from 'react';
 
 const MovieCard = ({ id, title, genre, release_date }) => {
+    const [smaller, setSmaller] = useState('');
     const date = new Intl.DateTimeFormat('en-US').format(new Date(release_date))
+
+    const handleLeave = () => {
+        setSmaller('smaller')
+    }
 
     return <>
         <Link to={`/detail/${id}`} >
-            <div className='card'>
+            <div className={smaller +' card'} onMouseLeave={handleLeave}>
                 <span >Title: {title}</span>
                 <span>Genre: {genre}</span>
                 <span>Release Date: {date}</span>
