e29ce917f2a91906b14bbee23b1fc0c5605d6da6
Jayro Gómez <jaxjayro@gmail.com>
2023-03-06 14:18:46 -0700

    implementing lazy load adn suspense

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,21 +1,24 @@
-
+import { lazy, Suspense } from 'react'
 import { useMovies } from '../hooks/useMovies'
-import MovieCard from './MovieCard/MovieCard'
 import LoadingSpinner from './Spinner/Spinner'
+const MovieCard = lazy(() => import('./MovieCard/MovieCard'))
 
-const SearchResults = ({ params = ''}) => {
+
+const SearchResults = ({ params = '' }) => {
     const { search } = params
     const { movies, loading } = useMovies({ search })
     return <>
         {loading
             ? <LoadingSpinner />
             : movies.map(({ id, title, genre, release_date }) => {
-                return <MovieCard
-                    key={id}
-                    id = {id}
-                    title={title}
-                    genre={genre}
-                    release_date={release_date} />
+                return <Suspense key={id}>
+                    <MovieCard
+                        key={id}
+                        id={id}
+                        title={title}
+                        genre={genre}
+                        release_date={release_date} />
+                </Suspense>
             })
         }
     </>
