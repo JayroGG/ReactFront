644b8b5a199c49c516ecc5cf0eba274da5c969d5
Jayro Gómez <jaxjayro@gmail.com>
2023-03-11 00:45:43 -0700

    Implemented localStorage without revalidation

--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,5 +1,13 @@
 export const getMovies = async ({ url }) => {
-    const response = await fetch(url)
-    const { data } = await response.json()
-    return data
+    const localData = JSON.parse(localStorage.getItem(url))
+    if (!localData) {
+        const response = await fetch(url)
+        const { data, pages } = await response.json()
+        localStorage.setItem(url, JSON.stringify(data))
+        localStorage.setItem(url+'pages', JSON.stringify(pages))
+        console.log('fetch')
+        return data
+    }
+    console.log('local')
+    return localData
 }
