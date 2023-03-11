fd61b53b87b99fda58a6be076e222d49d7bb7993
Jayro Gómez <jaxjayro@gmail.com>
2023-03-11 00:46:42 -0700

    Adding a redux state to get the maximun pages

--- src/services/pageSlice.js
+++ src/services/pageSlice.js
@@ -3,7 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 const pageSlice = createSlice({
   name: 'pagination',
   initialState: {
-    page: 0
+    page: 0,
+    maxPages: 0
   },
   reducers: {
     nextPage: state => {
@@ -11,10 +12,13 @@ const pageSlice = createSlice({
     },
     prevPage: state => {
       state.page = state.page === 0 ? 0 : state.page - 1
+    },
+    setMaxPages: (state) => {
+      state.maxPages = 3
     }
   }
 })
 
-export const { nextPage, prevPage } = pageSlice.actions
+export const { nextPage, prevPage, setMaxPages } = pageSlice.actions
 
 export default pageSlice.reducer
